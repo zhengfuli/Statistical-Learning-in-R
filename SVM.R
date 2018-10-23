81f55b7d37369bcd69d2130d27d468394a6c0620
zhengfuli <zhengfuli@bupt.edu.cn>
2018-10-24 01:54:17 +0800

    SVM and kernelized SVM

---
+++ SVM.R
@@ -0,0 +1,40 @@
+library(ggplot2)
+
+p = ggplot() + geom_line(data = data.frame(X1 = c(-1, 1), X2 = c(1.5, 0.5)), aes(X1, X2)) +
+geom_text(x = c(-0.5, 0.5), y = c(0.75, 1.25), aes(label = label), data.frame(label = c("-2 + X1 + 2X2 < 0", "-2 + X1 + 2X2 > 0"))) +
+scale_x_continuous(breaks=seq(-1, 1, 1))
+
+
+f = function(x)
+{
+  y1 = 2 - sqrt(4 - (1 + x)^2)
+  y2 = 2 + sqrt(4 - (1 + x)^2)
+  d = data.frame(x, y1, y2)
+  return(d)
+}
+
+x = seq(-3, 1, 0.005)
+
+p = ggplot() + geom_line(data = f(x), aes(x, y1)) +
+geom_line(data = f(x), aes(x, y2)) + coord_cartesian(xlim = c(-4, 5), ylim = c(-1, 8)) +
+xlab("X1") + ylab("X2") +
+geom_text(x = c(-1, -1), y = c(2, 4.5), aes(label = label), data.frame(label = c("(1 + X1)^2 + (2-X2)^2 < 4", "(1 + X1)^2 + (2-X2)^2 > 4")))
+
+f1 = function(x, y)
+{
+  c = ""
+  if((1 + x)^2 + (2 - y)^2 < 4) c = "#D55E00"
+  else c = "#56B4E9"
+  return(c)
+}
+p = p + geom_point(data = data.frame(x = c(0, -1, 2, 3), y = c(0, 1, 2, 8)), aes(x, y), col = c(f1(0, 0), f1(-1, 1), f1(2, 2), f1(3, 8)), size = 3) + scale_x_continuous(breaks=seq(-4, 5, 1))
+# plot(p)
+
+observations = data.frame(X1 = c(3,2,4,1,2,4,4,2), X2 = c(4,2,4,4,1,3,1,3))
+col = c(rep("#D55E00", 4), rep("#56B4E9", 4))
+p = ggplot() + geom_point(data = observations, aes(X1, X2), col = col, size = 3) +
+    coord_cartesian(xlim = c(0, 5), ylim = c(0, 5))
+
+p = p + geom_line(data = data.frame(X1 = c(0.5, 5), X2 = c(0, 4.5)), aes(X1, X2)) +
+    geom_line(data = data.frame(X1 = c(0, 5), X2 = c(0, 5)), aes(X1, X2), linetype="dashed") +
+    geom_line(data = data.frame(X1 = c(1, 5), X2 = c(0, 4)), aes(X1, X2), linetype="dashed")
---
+++ SVM_gas_mileage.R
@@ -0,0 +1,51 @@
+library(ISLR)
+med = median(Auto$mpg)
+new_mpg = ifelse(Auto$mpg > med, 1, 0)
+Auto$new_mpg = as.factor(new_mpg)
+library(e1071)
+set.seed(1)
+attach(Auto)
+na.omit(Auto)
+# fix(Auto)
+# tuning = tune(svm, new_mpg ~ ., data = Auto, kernel = "linear", ranges = list(cost = c(.1, .2, .4, .6, .7,.71,.72,.73,.74, .75)))
+# print(summary(tuning))
+
+# tuning_radial = tune(svm, new_mpg ~ ., data = Auto, kernel = "radial", ranges = list(cost = c(100), gamma = c(0.01)))
+# print(summary(tuning_radial))
+
+# tuning_polynomial = tune(svm, new_mpg ~ ., data = Auto, kernel = "polynomial", ranges = list(cost = c(122, 123, 123.5, 124, 124.5, 125, 126), degree = c(2, 3, 4)))
+# print(summary(tuning_polynomial))
+
+# svm.linear = svm(new_mpg ~ mpg + acceleration, data = Auto, kernel = "linear", cost = 0.72)
+
+# svm.polynomial = svm(new_mpg ~ ., data = Auto, kernel = "polynomial", cost = 100, degree = 2)
+# plot(svm.linear, Auto, mpg ~ acceleration, svSymbol="#")
+# plotpairs = function(fit) {
+#     for (name in names(Auto)[!(names(Auto) %in% c("mpg", "mpglevel", "name"))])
+#     {
+#         plot(fit, Auto, as.formula(paste("mpg~", name, sep = "")))
+#     }
+# }
+#
+# plotpairs(svm.linear)
+
+svm_plot = function(X1, X2, kernel, cost, gamma, degree)
+{
+    fit = svm(as.formula(paste("new_mpg ~", X1, "+", X2)), data = Auto, kernel = kernel, cost = cost, degree = degree)
+
+    grid_range = apply(Auto[c(X1, X2)], 2, range)
+    grid_X1 = seq(from = grid_range[1, 1] - 0.25, to = grid_range[2, 1] + 0.25, length = 75)
+    grid_X2 = seq(from = grid_range[1, 2] - 0.5, to = grid_range[2, 2] + 0.5, length = 75)
+
+    grid = expand.grid(acceleration = grid_X1, weight = grid_X2)
+    grid$class = predict(fit, grid)
+    decision_values = predict(fit, grid, decision.values = TRUE)
+    grid$z = as.vector(attributes(decision_values)$decision)
+
+    plot(grid[c(X1, X2)], col = ifelse(grid$class == 1, '#0571B070', '#CA002070'), pch='20', cex=.2)
+    points(Auto[c(X1, X2)], col = ifelse(new_mpg == 1, '#0571B070', '#CA002070'))
+    contour(grid_X1, grid_X2, matrix(grid$z, length(grid_X1), length(grid_X2)), level = 0, lwd = 1.5, drawlabels = FALSE, add = TRUE)
+    mtext(paste('\nKernel:', kernel, '        Cost:', cost, '        Degree:', degree), line = -3, outer = TRUE)
+}
+
+svm_plot("acceleration", "weight", "polynomial", cost = 124, degree = 2)
